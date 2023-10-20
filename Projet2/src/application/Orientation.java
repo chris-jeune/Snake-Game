10d78fa372f68211a31aadb3c965f36786e39ccc
Christian Jeune <145855247+chris-jeune@users.noreply.github.com>
2023-10-19 22:38:38 -0400

    Add files via upload

---
+++ Projet2/src/application/Orientation.java
@@ -0,0 +1,12 @@
+package application;
+
+/**
+ * Contient les différentes directions dans lesquelles se dirige le serpent
+ * 
+ * 
+ * @author Christian Jeune
+ * @version 1.0
+ */
+public enum Orientation {
+	UP, RIGHT, DOWN, LEFT
+}
